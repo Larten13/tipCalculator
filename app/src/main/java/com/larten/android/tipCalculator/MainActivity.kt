a3cc204d09286e4f669151deaa3154caba172664
larten13 <ya.dimanys@mail.ru>
2021-12-07 17:27:00 +0400

    Еврейский калькулятор с учетом 4-х пользователей, косяк в округлениях(насчитывает лишние деньги за счет округления и чая, и суммы с человека)

--- app/src/main/java/com/larten/android/tipCalculator/MainActivity.kt
+++ app/src/main/java/com/larten/android/tipCalculator/MainActivity.kt
@@ -15,28 +15,53 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.calculateButton.setOnClickListener { calculateTip() }
+        binding.calculateButton.setOnClickListener { displayTip() }
     }
 
-    private fun calculateTip() {
+    private fun calculateCost(): Double {
         val stringInTextField = binding.costOfService.text.toString()
         val cost = stringInTextField.toDoubleOrNull()
-        if (cost == null || cost == 0.0) {
-            displayTip(0.0)
-            return
+        return if (cost == null || cost == 0.0) {
+            0.0
+        } else cost
+    }
+    private fun numOfUser(): Int {
+        val numOfUser = when (binding.userOptions.checkedRadioButtonId) {
+            R.id.user1 -> 1
+            R.id.user2 -> 2
+            R.id.user3 -> 3
+            else -> 4
         }
+        return numOfUser
+    }
+    private fun calculateSumOfUser(): Double {
+        val cost = calculateCost()
+        val numOfUser = numOfUser()
+        var sumOfUser = cost / numOfUser
+        if (binding.roundUpSwitch.isChecked) sumOfUser = ceil(sumOfUser)
+        return sumOfUser
+    }
+    private fun calculateTipOfUser(): Double {
+        val cost = calculateCost()
+        val numOfUser = numOfUser()
         val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
-            R.id.option_eighteen_percent -> 0.18
-            R.id.option_fifteen_percent -> 0.15
-            else -> 0.2
+            R.id.option_five_percent -> 0.05
+            R.id.option_three_percent -> 0.03
+            else -> 0.1
         }
-        var tip = cost * tipPercentage
-        if (binding.roundUpSwitch.isChecked) tip = ceil(tip)
-        displayTip(tip)
+        var tipOfUser = cost * tipPercentage / numOfUser
+        if (binding.roundUpSwitch.isChecked) tipOfUser = ceil(tipOfUser)
+        return tipOfUser
     }
 
-    fun displayTip(tip: Double) {
-        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
-        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+
+    fun displayTip() {
+        val tip = calculateTipOfUser()
+        val sumOfUser = calculateSumOfUser()
+        val summaryOfUser = tip + sumOfUser
+        val formattedSum = NumberFormat.getCurrencyInstance().format(summaryOfUser)
+        binding.tipResult.text = getString(R.string.tip_amount, formattedSum)
+//        binding.tipTest.text = tip.toString()
+//        binding.tipTest2.text = sumOfUser.toString()
     }
 }
